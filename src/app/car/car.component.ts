976f0405b4646cfe9370e1b34bafe775aa9558df
nik <worldnillin@gmail.com>
2018-02-02 17:07:54 +0300

    3.5 Передача HTML кода компоненту

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, ContentChild, ElementRef, ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -8,5 +8,9 @@ import { Component, Input } from '@angular/core';
 export class CarComponent {
 
   @Input() carItem: {name: string, year: number};
+  @ContentChild('carHeading') carHeading: ElementRef;
 
+  ngAfterViewInit() {
+    console.log(this.carHeading);
+  }
 }
